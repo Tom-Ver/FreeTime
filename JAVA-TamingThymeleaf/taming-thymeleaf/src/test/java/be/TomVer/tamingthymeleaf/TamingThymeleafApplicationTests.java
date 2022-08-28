969cea06c0cb057d7a3d98ae5c72fae9745df403
Tom-Ver <LW8twQRKghWbspj6x6KyzmfrQFp>
2022-08-28 14:59:13 +0200

    Started Taming Thymeleaf

---
+++ JAVA-TamingThymeleaf/taming-thymeleaf/src/test/java/be/TomVer/tamingthymeleaf/TamingThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package be.TomVer.tamingthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamingThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
